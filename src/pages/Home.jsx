d35a82e48c4a1219f3034740a31fffea453c971b
admin20 <admin@admins-MacBook-Pro.local>
2025-08-10 22:01:19 +0530

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeroSection from '../components/HeroSection'
+
+const home = () => {
+  return (
+    <>
+      <HeroSection />
+    </>
+  )
+}
+
+export default home
