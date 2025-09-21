de9a965b81e29dc65bf53f2a74b555c3aaa9dcb6
admin20 <admin@admins-MacBook-Pro.local>
2025-09-22 00:03:11 +0530

    bug fix

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { dummyBookingData } from '../assets/assets'
+
 import Loading from '../components/Loading'
 import BlurCircle from '../components/BlurCircle'
 import timeFormat from '../lib/timeFormat'
@@ -18,7 +18,7 @@ const {axios,getToken,user,image_base_url} = useAppContext()
 
   const getMyBooking = async () => {
    try{
- const { data } = await axios.post('/api/user/bookings',{headers: {Authorizaton: `Bearer ${await getToken()}`}})
+ const { data } = await axios.post('/api/user/bookings',{},{headers: {Authorizaton: `Bearer ${await getToken()}`}})
  if(data.success){
   setbookings(data.bookings)
  }
@@ -31,7 +31,7 @@ setIsLoading(false)
     if(user){
     getMyBooking()
   }
-  }, [])
+  }, [user])
   return !isLoading ? (
     <div className='relative px-6 md:px-16 lg:px-40 pt-30 md:pt-40 min-h-[80vh]'>
       <BlurCircle top="100px" left="100px" />
@@ -42,7 +42,7 @@ setIsLoading(false)
 
 
       {bookings.map((item, index) => (
-        <div key={item.id} className='flex flex-col md:flex-row justify-between bg-primary/8 border border-primary/20 rounded-lg mt-4 p-2 max-w-3xl'>
+        <div key={item._id} className='flex flex-col md:flex-row justify-between bg-primary/8 border border-primary/20 rounded-lg mt-4 p-2 max-w-3xl'>
           <div className='flex flex-col md:flex-row'>
             <img src={image_base_url + item.show.movie.poster_path} alt="" className='md:max-w-45 aspect-video h-auto object-cover object-bottom rounded' />
             <div className='flex flex-col p-4'>
@@ -54,7 +54,7 @@ setIsLoading(false)
           <div className='flex flex-col md:items-end md:text-right justify-between p-4'>
             <div className='flex items-center gap-4'>
               <p className='text-2xl font-semibold mb-3'>{currency}{item.amount}</p>
-              {!item.isPaid && <button className='bg-primary px-4 py-1.5 mb-3 text-sm rounded-full font-medium cursor-pointer'>Pay Now</button>}
+              {!item.isPaid && <Link  to={item.paymentLink} className='bg-primary px-4 py-1.5 mb-3 text-sm rounded-full font-medium cursor-pointer'>Pay Now</Link>}
             </div>
             <div className='text-sm'>
               <p><span className='text-gray-400'>Total Ticket:</span>{item.bookedSeats.length}</p>
