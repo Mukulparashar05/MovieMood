db085358b2c01b9481348b2b44530ba09248bcbf
admin20 <admin@admins-MacBook-Pro.local>
2025-09-21 23:52:56 +0530

    function added to release seat after 10 minutes

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,6 +1,7 @@
 import Show from "../models/Show.js"
 import Booking from "../models/Booking.js";
 import stripe from 'stripe'
+import { inngest } from "../inngest/index.js";
 
 
 //Function to check availability of the selected seats for a movie
@@ -81,6 +82,13 @@ const session = await stripeInstance.chechout.sessions.create({
 booking.paymentLink= session.url
 await booking.save()
 
+// Run Inngest sheduler function to check payment status after 10 minutes
+await inngest.send({
+    name:"app/checkpayment",
+    data:{
+        bookingId: booking._id.toString()
+    }
+})
 res.json({success:true,url:session.url})
     }catch(error){
         console.log(error.message);
